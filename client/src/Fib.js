97e5e58c298a90e5daf829117193d8715e0e6b0c
Aninda Kar <aninda.kar@outlook.com>
2020-08-09 19:09:16 +0530

    added the react app logic

---
+++ client/src/Fib.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default Fib = () => {
+  const [seenIndexes, setSeenIndexes] = useState([]);
+  const [values, setValues] = useState({});
+  const [index, setIndex] = useState('');
+
+  const fetchValues = () => {
+
+  }
+
+  const fetchIndexes = () => {
+
+  }
+
+  const handleSubmit = (event) => {
+    setIndex(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Index value submitted');
+  }
+
+  useEffect(() => {
+    fetchValues();
+    fetchIndexes();    
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter Index: </label>
+        <input 
+          name="index"
+          value={index}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <h3>Indexes I have seen:</h3>
+
+      <h3>Calculated values:</h3>
+    </div>
+  );
+};
